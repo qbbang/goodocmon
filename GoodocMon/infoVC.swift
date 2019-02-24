04bfdb9f9326773fce17a4bdf078b0d30bbbf8ae
이규현 <qbbang@gmail.com>
2019-02-25 08:53:42 +0900

    Add, 굿닥몬고 초기세팅

---
+++ GoodocMon/infoVC.swift
@@ -0,0 +1,30 @@
+//
+//  infoVC.swift
+//  GoodocMon
+//
+//  Created by qbbang on 23/02/2019.
+//  Copyright © 2019 qbbang. All rights reserved.
+//
+
+import UIKit
+
+class infoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GoodocMon' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GoodocMon
+  pod 'lottie-ios'
+
+end
